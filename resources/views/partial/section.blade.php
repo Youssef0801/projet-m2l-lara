509da806a8a2a7915498dd755216cea97fa396c3
Youssef0801 <assehnoune.youssef@gmail.com>
2025-06-23 02:27:40 +0200

    Mise à jour de la description du projet dans la section de présentation

--- resources/views/partial/section.blade.php
+++ resources/views/partial/section.blade.php
@@ -1,8 +1,8 @@
 <section>
             <h2><span class="align-v" aria-hidden="true">💻</span>Présentation du projet</h2>
-            <p>In four days, at the latest, the 'Great Eastern' must reach New York harbour;
-             therefore we might hope that accident would not dally with our watchfulness, and that Fabian would not discover Ellen during this time; but we made our 
-             calculations without thinking of events.</p>
+            <p>Maison de Ligue est une application web professionnelle conçue pour faciliter la gestion des employés 
+                d'une ligue. Elle offre une interface intuitive et conviviale permettant aux administrateurs de gérer efficacement 
+                les informations relatives aux employés, telles que leurs informations personnelles, leurs rôles, leurs contrats, etc</p>
              <a href="{{ url("/presentation")}}" class="btn-default btn-success">Présentation</a>
              <a href="{{ url("/contact")}}" class="btn-default btn-success">Contact</a>
              <a href="{{ route('login') }}" class="btn-default btn-success">Se connecter</a>
