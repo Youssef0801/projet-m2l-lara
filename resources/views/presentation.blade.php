768b0b8daad68c23079463b6a63be742ff03f628
Youssef0801 <assehnoune.youssef@gmail.com>
2025-06-24 00:35:52 +0200

    Suppression de la section sur les langages de programmation dans la présentation

--- resources/views/presentation.blade.php
+++ resources/views/presentation.blade.php
@@ -19,42 +19,7 @@
              <a href="liste" class="btn-default btn-secondary">Search in list</a>
         </section>
 
-    <section>
-            <h2>Langages de programmation</h2>
-    <table>
- {{--    <caption>
-        La liste des donéees
-    </caption> --}}   
-        <thead>
-            <tr>
-                <th>Langage</th>
-                <th>Type</th> 
-                <th>Version</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($lang as $item)
-          {{--   {{ dd($lang) }} --}}
-            <tr>
-                <td>{{ $item['lang']}}</td>
-                <td>{{ $item['type']}}</td>
-                <td>{{ $item['version']}}</td>
-            </tr>
-         
-        @endforeach
-        </tbody>
-    </table>
-                
-             {{--    <ul class="list-lang">
-                @foreach ($lang as $item)
-                    <li>{{ $item['lang']}}, type : {{ $item['type']}}, Version : {{ $item['version']}}</li>
-                    
-                @endforeach
-                    
-                </ul> --}}
-           
-          
-        </section>
+    
     </main>
     @include('partial.footer')
         <script type="module" src="{{ Vite::asset('resources/js/app.js') }}"></script>
